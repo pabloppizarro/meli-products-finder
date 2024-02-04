d131bd5a8cb5ed98d095da4ebe3e4be54c2c3974
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-04 13:52:18 -0300

    refactor item list cards styles and html structure

--- src/app/items/_components/ItemCard.tsx
+++ src/app/items/_components/ItemCard.tsx
@@ -5,23 +5,35 @@ import styles from "@/styles/components/item-card.module.scss";
 export default function ListItem({ item }: { item: IItem }) {
   return (
     <article className={styles.item}>
-      <div className={styles.image}>
-        <Image src={item.picture} fill={true} alt={`Foto de ${item.title}`} />
-      </div>
-      <div className="description">
-        <div className={styles.price}>
-          <h3>
-            {item.price.amount.toLocaleString("es-ar", {
-              style: "currency",
-              currency: item.price.currency,
-              minimumFractionDigits: item.price.decimals,
-            })}
-            <span>{item.free_shipping ? "🚚" : ""}</span>
-          </h3>
+      <div className={styles.wrapper}>
+        <div className={styles.image}>
+          <div className={styles.imageContainer}>
+            <figure>
+              <div className={styles.overlay}></div>
+              <Image
+                src={item.picture}
+                fill={true}
+                alt={`Foto de ${item.title}`}
+              />
+            </figure>
+          </div>
         </div>
-        <h2>{item.title}</h2>
-        {/* <Link href={`items/${item.id}`}>
+        <div className={styles.description}>
+          <div className={styles.price}>
+            <h2>{item.title}</h2>
+            <h3>
+              {item.price.amount.toLocaleString("es-ar", {
+                style: "currency",
+                currency: item.price.currency,
+                minimumFractionDigits: item.price.decimals,
+              })}
+              <span>{item.free_shipping ? "🚚" : ""}</span>
+            </h3>
+          </div>
+
+          {/* <Link href={`items/${item.id}`}>
         </Link> */}
+        </div>
       </div>
     </article>
   );
