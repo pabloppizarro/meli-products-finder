e00a616d47a400adc384157b84c664cc0db966b5
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-05 11:10:55 -0300

    refactor pages desktop sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from "@/styles/pages/page.module.scss";
 export default function Home() {
   return (
-    <section className={styles.home}>
-      <p>Busca productos para comenzar.</p>
+    <section className={styles.homePage}>
+      <div className={styles.content}>
+        <p>Busca productos para comenzar.</p>
+      </div>
     </section>
   );
 }
