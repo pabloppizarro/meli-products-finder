496882ff3cce6b2c6c4974d945e179a34f099fa3
pabloppizarro <pizarro.pablo@outlook.com>
2024-02-05 14:56:15 -0300

    added error page: redirect to home

--- src/app/items/error.tsx
+++ src/app/items/error.tsx
@@ -1,7 +1,7 @@
 "use client"; // Error components must be Client Components
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-
 export default function Error({
   error,
   reset,
@@ -9,6 +9,7 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error);
@@ -20,10 +21,10 @@ export default function Error({
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => router.push("/")
         }
       >
-        Intentar de nuevo.
+        Ir al inicio.
       </button>
     </div>
   );
